99050b1f862bcb4f7ccb69690bf0c950940c7139
Julien Finet <julien.finet@kitware.com>
2013-05-17 19:19:17 -0400

    COMP: Fix Slicer_QTSCRIPTEDMODULES_DISABLED

--- SuperBuild.cmake
+++ SuperBuild.cmake
@@ -280,7 +280,7 @@ endif()
 
 if(Slicer_BUILD_QTSCRIPTEDMODULES)
   # Provide a mechanism to disable one or more scripted modules.
-  list(APPEND eq_superbuild_extra_args -DSlicer_QTSCRIPTEDMODULES_DISABLED:STRING=${Slicer_QTSCRIPTEDMODULES_DISABLED})
+  list(APPEND ep_superbuild_extra_args -DSlicer_QTSCRIPTEDMODULES_DISABLED:STRING=${Slicer_QTSCRIPTEDMODULES_DISABLED})
 endif()
 
 if(Slicer_USE_BatchMake)
