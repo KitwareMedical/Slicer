KitwareMedical/Slicer

commit 3bf8417efd7b949610a8a3765d058e8d3997135f

ENH: added 3d views linking capability
Added Link button in 3DViewController;
Added MRMLViewLogic: set the interactions;
Addes MRMLViewLinkLogic: propagate the events.

next step for Slicer 4.10 (no backward compatibility): merge vtkMRMLCameraNode into vtkMRMLViewNode

Co-authored-by: Davide Punzo <punzodavide@hotmail.it>
svn-url: http://viewvc.slicer.org/viewvc.cgi/Slicer4?view=revision&revision=27297
git-svn-id: http://svn.slicer.org/Slicer4/trunk@27297 3bd1e089-480b-0410-8dfb-8563597acbee

Libs/MRML/Core/vtkMRMLCameraNode.cxx
@@ -53,6 +53,9 @@ vtkMRMLCameraNode::vtkMRMLCameraNode()
   this->SetAndObserveCamera(camera.GetPointer());
 
   this->AppliedTransform = vtkMatrix4x4::New();
+
+  this->Interacting = 0;
+  this->InteractionFlags = 0;
  }
 
 //----------------------------------------------------------------------------
@@ -214,6 +217,7 @@ void vtkMRMLCameraNode::PrintSelf(ostream& os, vtkIndent indent)
   vtkMRMLPrintFloatMacro(ParallelScale);
   vtkMRMLPrintFloatMacro(ViewAngle);
   vtkMRMLPrintStringMacro(ActiveTag);
+  vtkMRMLPrintIntMacro(Interacting);
   vtkMRMLPrintEndMacro();
 
   os << indent << "AppliedTransform: " ;
@@ -337,6 +341,7 @@ void vtkMRMLCameraNode::ProcessMRMLEvents ( vtkObject *caller,
       event ==  vtkCommand::ModifiedEvent)
     {
     this->Modified();
+    this->InvokeCustomModifiedEvent(vtkMRMLCameraNode::CameraModifiedEvent, caller);
     }
 
   vtkMRMLTransformNode *tnode = this->GetParentTransformNode();
@@ -688,4 +693,18 @@ void vtkMRMLCameraNode::Reset(bool resetRotation,
     renderer->UpdateLightsGeometryToFollowCamera();
     }
   this->EndModify(wasModifying);
+}
+
+//-----------------------------------------------------------
+void vtkMRMLCameraNode::SetInteracting(int interacting)
+{
+  // Don't call Modified()
+  this->Interacting = interacting;
+}
+
+//-----------------------------------------------------------
+void vtkMRMLCameraNode::SetInteractionFlags(unsigned int flags)
+{
+  // Don't call Modified()
+  this->InteractionFlags = flags;
 }

Base/QTGUI/qSlicerApplication.cxx
@@ -477,6 +477,8 @@ void qSlicerApplication::setLayoutManager(qSlicerLayoutManager* layoutManager)
     {
     this->applicationLogic()->SetSliceLogics(
       d->LayoutManager? d->LayoutManager.data()->mrmlSliceLogics() : 0);
+    this->applicationLogic()->SetViewLogics(
+      d->LayoutManager? d->LayoutManager.data()->mrmlViewLogics() : 0);
     if (d->LayoutManager)
       {
       d->LayoutManager.data()->setMRMLColorLogic(this->applicationLogic()->GetColorLogic());

Libs/MRML/Core/vtkMRMLCameraNode.h
@@ -148,7 +148,9 @@ public:
   /// Events
   enum
   {
-    ActiveTagModifiedEvent = 30000
+    ActiveTagModifiedEvent = 30000,
+    CameraModifiedEvent = 31000,
+    ResetCameraClippingEvent = 32000,
   };
 
   /// Mark the active tag node as references.
@@ -213,6 +215,39 @@ public:
              bool resetTranslation = true,
              bool resetDistance = true,
              vtkRenderer* renderer = 0);
+
+  /// Get/Set a flag indicating whether this node is actively being
+  /// manipulated (usually) by a user interface. This flag is used by
+  /// logic classes to determine whether state changes should be
+  /// propagated to other nodes to implement linked controls. Does not
+  /// cause a Modified().
+  void SetInteracting(int);
+  vtkGetMacro(Interacting, int);
+  vtkBooleanMacro(Interacting, int);
+
+  /// Enum identifying the parameters being manipulated with calls to
+  /// InteractionOn() and InteractionOff(). Identifiers are powers of
+  /// two so they can be combined into a bitmask to manipulate
+  /// multiple parameters.
+  /// The meanings for the flags are:
+  enum InteractionFlagType
+  {
+    None = 0,
+    LookFromAxis,
+    ZoomInFlag,
+    ZoomOutFlag,
+    CenterFlag,
+    vtkCameraFlag,
+  };
+
+  /// Get/Set a flag indicating what parameters are being manipulated
+  /// within calls to InteractingOn() and InteractingOff(). These
+  /// fields are used to propagate linked behaviors. This flag is a
+  /// bitfield, with multiple parameters OR'd to compose the
+  /// flag. Does not cause a Modifed().
+  void SetInteractionFlags(unsigned int);
+  vtkGetMacro(InteractionFlags, unsigned int);
+
 protected:
   vtkMRMLCameraNode();
   ~vtkMRMLCameraNode();
@@ -230,6 +265,9 @@ protected:
   char *InternalActiveTag;
 
   vtkMatrix4x4 *AppliedTransform;
+
+  int Interacting;
+  unsigned int InteractionFlags;
 };
 
 //---------------------------------------------------------------------------

Libs/MRML/Core/vtkMRMLViewNode.cxx
@@ -61,6 +61,9 @@ vtkMRMLViewNode::vtkMRMLViewNode()
   this->RaycastTechnique = vtkMRMLViewNode::Composite;
   this->VolumeRenderingSurfaceSmoothing = false;
   this->VolumeRenderingOversamplingFactor = 2.0;
+  this->LinkedControl = 0;
+  this->Interacting = 0;
+  this->InteractionFlags = 0;
 }
 
 //----------------------------------------------------------------------------
@@ -105,6 +108,7 @@ void vtkMRMLViewNode::WriteXML(ostream& of, int nIndent)
   vtkMRMLWriteXMLEnumMacro(raycastTechnique, RaycastTechnique);
   vtkMRMLWriteXMLIntMacro(volumeRenderingSurfaceSmoothing, VolumeRenderingSurfaceSmoothing);
   vtkMRMLWriteXMLFloatMacro(volumeRenderingOversamplingFactor, VolumeRenderingOversamplingFactor);
+  vtkMRMLWriteXMLIntMacro(rockLength, LinkedControl);
   vtkMRMLWriteXMLEndMacro();
 }
 
@@ -140,6 +144,7 @@ void vtkMRMLViewNode::ReadXMLAttributes(const char** atts)
   vtkMRMLReadXMLEnumMacro(raycastTechnique, RaycastTechnique);
   vtkMRMLReadXMLIntMacro(volumeRenderingSurfaceSmoothing, VolumeRenderingSurfaceSmoothing);
   vtkMRMLReadXMLFloatMacro(volumeRenderingOversamplingFactor, VolumeRenderingOversamplingFactor);
+  vtkMRMLReadXMLIntMacro(LinkedControl, LinkedControl)
   vtkMRMLReadXMLEndMacro();
 
   this->EndModify(disabledModify);
@@ -179,6 +184,7 @@ void vtkMRMLViewNode::Copy(vtkMRMLNode *anode)
   vtkMRMLCopyIntMacro(RaycastTechnique);
   vtkMRMLCopyIntMacro(VolumeRenderingSurfaceSmoothing);
   vtkMRMLCopyFloatMacro(VolumeRenderingOversamplingFactor);
+  vtkMRMLCopyIntMacro(LinkedControl);
   vtkMRMLCopyEndMacro();
 
   this->EndModify(disabledModify);
@@ -214,6 +220,8 @@ void vtkMRMLViewNode::PrintSelf(ostream& os, vtkIndent indent)
   vtkMRMLPrintIntMacro(RaycastTechnique);
   vtkMRMLPrintIntMacro(VolumeRenderingSurfaceSmoothing);
   vtkMRMLPrintFloatMacro(VolumeRenderingOversamplingFactor);
+  vtkMRMLPrintIntMacro(Interacting);
+  vtkMRMLPrintIntMacro(LinkedControl);
   vtkMRMLPrintEndMacro();
 }
 
@@ -481,4 +489,18 @@ int vtkMRMLViewNode::GetRaycastTechniqueFromString(const char* name)
     }
   // unknown name
   return -1;
+}
+
+//-----------------------------------------------------------
+void vtkMRMLViewNode::SetInteracting(int interacting)
+{
+  // Don't call Modified()
+  this->Interacting = interacting;
+}
+
+//-----------------------------------------------------------
+void vtkMRMLViewNode::SetInteractionFlags(unsigned int flags)
+{
+  // Don't call Modified()
+  this->InteractionFlags = flags;
 }

Libs/MRML/Core/vtkMRMLViewNode.h
@@ -248,6 +248,51 @@ public:
     ResetFocalPointRequestedEvent,
     };
 
+  /// Get/Set a flag indicating whether this node is actively being
+  /// manipulated (usually) by a user interface. This flag is used by
+  /// logic classes to determine whether state changes should be
+  /// propagated to other nodes to implement linked controls. Does not
+  /// cause a Modified().
+  void SetInteracting(int);
+  vtkGetMacro(Interacting, int);
+  vtkBooleanMacro(Interacting, int);
+
+  /// Enum identifying the parameters being manipulated with calls to
+  /// InteractionOn() and InteractionOff(). Identifiers are powers of
+  /// two so they can be combined into a bitmask to manipulate
+  /// multiple parameters.
+  /// The meanings for the flags are:
+  /// RenderModeFlag - broadcast the RenderMode to all linked viewers
+  enum InteractionFlagType
+  {
+    None = 0,
+    AnimationModeFlag,
+    RenderModeFlag,
+    BoxVisibleFlag,
+    BoxLabelVisibileFlag,
+    BackgroundColorFlag,
+    StereoTypeFlag,
+    OrientationMarkerTypeFlag,
+    OrientationMarkerSizeFlag,
+    RulerTypeFlag,
+    UseDepthPeelingFlag,
+    FPSVisibleFlag,
+  };
+
+  ///
+  /// toggle the view linking
+  vtkGetMacro (LinkedControl, int );
+  vtkSetMacro (LinkedControl, int );
+  vtkBooleanMacro(LinkedControl, int);
+
+  /// Get/Set a flag indicating what parameters are being manipulated
+  /// within calls to InteractingOn() and InteractingOff(). These
+  /// fields are used to propagate linked behaviors. This flag is a
+  /// bitfield, with multiple parameters OR'd to compose the
+  /// flag. Does not cause a Modifed().
+  void SetInteractionFlags(unsigned int);
+  vtkGetMacro(InteractionFlags, unsigned int);
+
 protected:
   vtkMRMLViewNode();
   ~vtkMRMLViewNode();
@@ -323,6 +368,11 @@ protected:
   /// factor.
   /// If \sa VolumeRenderingQuality is set to maximum quality, then a fix oversampling factor of 10 is used.
   double VolumeRenderingOversamplingFactor;
+
+  int LinkedControl;
+  int Interacting;
+  unsigned int InteractionFlags;
+
 };
 
 #endif

Libs/MRML/DisplayableManager/vtkMRMLCameraDisplayableManager.cxx
@@ -208,10 +208,13 @@ void vtkMRMLCameraDisplayableManager::ProcessMRMLNodesEvents(vtkObject *caller,
       this->UpdateCameraNode();
       vtkDebugMacro("ProcessingMRML: got a camera node modified event");
       break;
-    case vtkCommand::ResetCameraClippingRangeEvent:
+    case vtkMRMLCameraNode::ResetCameraClippingEvent:
+      assert(vtkMRMLCameraNode::SafeDownCast(caller));
+      vtkDebugMacro("ProcessingMRML: got a camera node modified event");
       if (this->GetRenderer())
         {
         this->GetRenderer()->ResetCameraClippingRange();
+        this->GetRenderer()->UpdateLightsGeometryToFollowCamera();
         }
       else if (this->GetCameraNode() && this->GetCameraNode()->GetCamera())
         {
@@ -284,7 +287,7 @@ void vtkMRMLCameraDisplayableManager::SetAndObserveCameraNode(vtkMRMLCameraNode 
   vtkNew<vtkIntArray> cameraNodeEvents;
   cameraNodeEvents->InsertNextValue(vtkCommand::ModifiedEvent);
   cameraNodeEvents->InsertNextValue(vtkMRMLCameraNode::ActiveTagModifiedEvent);
-  cameraNodeEvents->InsertNextValue(vtkCommand::ResetCameraClippingRangeEvent);
+  cameraNodeEvents->InsertNextValue(vtkMRMLCameraNode::ResetCameraClippingEvent);
 
   vtkSetAndObserveMRMLNodeEventsMacro(
     this->Internal->CameraNode, newCameraNode, cameraNodeEvents.GetPointer());

Libs/MRML/Logic/CMakeLists.txt
@@ -77,6 +77,8 @@ set(MRMLLogic_SRCS
   vtkMRMLSliceLayerLogic.cxx
   vtkMRMLSliceLogic.cxx
   vtkMRMLSliceLinkLogic.cxx
+  vtkMRMLViewLogic.cxx
+  vtkMRMLViewLinkLogic.cxx
 
   # slicer's vtk extensions (filters)
   vtkImageLabelOutline.cxx

Libs/MRML/Logic/vtkMRMLApplicationLogic.cxx
@@ -24,9 +24,11 @@
 // MRMLLogic includes
 #include "vtkMRMLApplicationLogic.h"
 #include "vtkMRMLColorLogic.h"
+#include "vtkMRMLModelHierarchyLogic.h"
 #include "vtkMRMLSliceLogic.h"
 #include "vtkMRMLSliceLinkLogic.h"
-#include "vtkMRMLModelHierarchyLogic.h"
+#include "vtkMRMLViewLogic.h"
+#include "vtkMRMLViewLinkLogic.h"
 
 // MRML includes
 #include "vtkMRMLInteractionNode.h"
@@ -39,6 +41,7 @@
 #include "vtkMRMLStorageNode.h"
 #include "vtkMRMLSceneViewNode.h"
 #include "vtkMRMLTableViewNode.h"
+#include "vtkMRMLViewNode.h"
 
 // VTK includes
 #include <vtkCollection.h>
@@ -78,7 +81,9 @@ public:
   vtkSmartPointer<vtkMRMLSelectionNode>    SelectionNode;
   vtkSmartPointer<vtkMRMLInteractionNode>  InteractionNode;
   vtkSmartPointer<vtkCollection> SliceLogics;
+  vtkSmartPointer<vtkCollection> ViewLogics;
   vtkSmartPointer<vtkMRMLSliceLinkLogic> SliceLinkLogic;
+  vtkSmartPointer<vtkMRMLViewLinkLogic> ViewLinkLogic;
   vtkSmartPointer<vtkMRMLModelHierarchyLogic> ModelHierarchyLogic;
   vtkSmartPointer<vtkMRMLColorLogic> ColorLogic;
   std::string TemporaryPath;
@@ -93,6 +98,7 @@ vtkMRMLApplicationLogic::vtkInternal::vtkInternal(vtkMRMLApplicationLogic * exte
 {
   this->External = external;
   this->SliceLinkLogic = vtkSmartPointer<vtkMRMLSliceLinkLogic>::New();
+  this->ViewLinkLogic = vtkSmartPointer<vtkMRMLViewLinkLogic>::New();
   this->ModelHierarchyLogic = vtkSmartPointer<vtkMRMLModelHierarchyLogic>::New();
   this->ColorLogic = vtkSmartPointer<vtkMRMLColorLogic>::New();
 }
@@ -151,6 +157,7 @@ vtkMRMLApplicationLogic::vtkMRMLApplicationLogic()
 {
   this->Internal = new vtkInternal(this);
   this->Internal->SliceLinkLogic->SetMRMLApplicationLogic(this);
+  this->Internal->ViewLinkLogic->SetMRMLApplicationLogic(this);
   this->Internal->ModelHierarchyLogic->SetMRMLApplicationLogic(this);
   this->Internal->ColorLogic->SetMRMLApplicationLogic(this);
 }
@@ -296,6 +303,78 @@ GetSliceLogicByModelDisplayNode(vtkMRMLModelDisplayNode* displayNode) const
 }
 
 //---------------------------------------------------------------------------
+void vtkMRMLApplicationLogic::SetViewLogics(vtkCollection *viewLogics)
+{
+  if (viewLogics == this->Internal->ViewLogics)
+    {
+    return;
+    }
+  this->Internal->ViewLogics = viewLogics;
+  this->Modified();
+}
+
+//---------------------------------------------------------------------------
+vtkCollection *vtkMRMLApplicationLogic::GetViewLogics() const
+{
+  return this->Internal->ViewLogics;
+}
+
+//---------------------------------------------------------------------------
+vtkMRMLViewLogic *vtkMRMLApplicationLogic::
+GetViewLogic(vtkMRMLViewNode *viewNode) const
+{
+  if(!viewNode || !this->Internal->ViewLogics)
+    {
+    return 0;
+    }
+
+  vtkMRMLViewLogic* logic = 0;
+  vtkCollectionSimpleIterator it;
+  vtkCollection* logics = this->Internal->ViewLogics;
+
+  for (logics->InitTraversal(it);
+      (logic=vtkMRMLViewLogic::SafeDownCast(logics->GetNextItemAsObject(it)));)
+    {
+    if (logic->GetViewNode() == viewNode)
+      {
+      break;
+      }
+
+    logic = 0;
+    }
+
+  return logic;
+}
+
+//---------------------------------------------------------------------------
+vtkMRMLViewLogic *vtkMRMLApplicationLogic::
+GetViewLogicByLayoutName(const char *layoutName) const
+{
+  if(!layoutName || !this->Internal->ViewLogics)
+    {
+    return 0;
+    }
+
+  vtkMRMLViewLogic* logic = 0;
+  vtkCollectionSimpleIterator it;
+  vtkCollection* logics = this->Internal->ViewLogics;
+
+  for (logics->InitTraversal(it);
+      (logic=vtkMRMLViewLogic::SafeDownCast(logics->GetNextItemAsObject(it)));)
+    {
+    if (logic->GetViewNode())
+      {
+      if ( !strcmp( logic->GetViewNode()->GetLayoutName(), layoutName) )
+        {
+        return logic;
+        }
+      }
+    }
+
+  return 0;
+}
+
+//---------------------------------------------------------------------------
 void vtkMRMLApplicationLogic::SetMRMLSceneInternal(vtkMRMLScene *newScene)
 {
   vtkMRMLNode * selectionNode = 0;
@@ -330,6 +409,7 @@ void vtkMRMLApplicationLogic::SetMRMLSceneInternal(vtkMRMLScene *newScene)
   this->Superclass::SetMRMLSceneInternal(newScene);
 
   this->Internal->SliceLinkLogic->SetMRMLScene(newScene);
+  this->Internal->ViewLinkLogic->SetMRMLScene(newScene);
   this->Internal->ModelHierarchyLogic->SetMRMLScene(newScene);
 }
 

commit fd28b694aebbdd5c8a159175fa854dce94d79566

[cell-locator] BUG: Fix vtkMRMLCameraNode::ResetClippingRange
This commit fixes a regression introduced in r27297 (ENH: added 3d views
linking capability) where use of "vtkCommand::ResetCameraClippingRangeEvent"
was replaced by custom event "vtkMRMLCameraNode::ResetCameraClippingEvent"

Libs/MRML/Core/vtkMRMLCameraNode.cxx
@@ -512,7 +512,7 @@ vtkMRMLCameraNode* vtkMRMLCameraNode::FindActiveTagInScene(const char* tag)
 //---------------------------------------------------------------------------
 void vtkMRMLCameraNode::ResetClippingRange()
 {
-  this->InvokeEvent(vtkCommand::ResetCameraClippingRangeEvent);
+  this->InvokeEvent(vtkMRMLCameraNode::ResetCameraClippingEvent);
 }
 
 //---------------------------------------------------------------------------